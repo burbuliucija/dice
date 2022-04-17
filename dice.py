99b538a9236f7b733c7aff968c3150a680d186c0
burbuliucija <94080972+paulinaad2@users.noreply.github.com>
2022-04-17 16:27:38 +0100

    add first version of the code

--- dice.py
+++ dice.py
@@ -11,6 +11,37 @@ x - number of dice to roll
 y - number of sides on the dice
 Example: 1d20 rolls 1 dice with 20 sides.''')
       return user_input()
+   else:
+      try:
+         if action[0] == "d":
+            dice_amount = 1
+            dice_sides = int(action[1:])
+         else:
+            action = action.split("d")
+            dice_amount = int(action[0])
+            dice_sides = int(action[1])
+         assert dice_amount > 0
+         assert dice_sides > 1
+      except:
+         print("Invalid syntax. Please try again.")
+         return user_input()
+      roll(dice_amount, dice_sides)
+
+def roll(amount, sides):
+   print(f'Rolling {amount}d{sides}...')
+   if amount == 1:
+      print(f'{random.randint(1, sides)}')
+   else:
+      rolls = []
+      sum = 0
+      for i in range(0, amount):
+         roll = random.randint(1, sides)
+         sum += roll
+         rolls.append(str(roll))
+      output = "+".join(rolls)
+      print(f'{output} = {sum}')
+      return user_input
+      
 
 print(f''' 
       Welcome to
